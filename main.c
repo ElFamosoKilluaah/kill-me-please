34ef940451463b60f0c9215d5d70413fd2345940
ElFamosoKilluaah <elfamosoquentin@gmail.com>
2019-04-23 19:02:58 +0200

    kill me

---
+++ main.c
@@ -0,0 +1,3 @@
+#include "main.h"
+
+please kill me i cant live anymore
---
+++ main.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#define please  int
+#define kill    main
+#define me      (
+#define i       )
+#define cant    {
+#define live    printf("kill me");
+#define anymore }
